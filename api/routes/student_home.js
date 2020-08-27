4a38505ec54331d9980b10ad83f983f39dcf6ef8
Nong Minh Hieu <hieubkvn123@gmail.com>
2020-08-27 14:48:48 +0800

    Added the api

--- api/routes/student_home.js
+++ api/routes/student_home.js
@@ -1,7 +1,52 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const formidable = require('formidable')
+
+const db_uri = 'mongodb://localhost:27017/Hieu'
 
 router = express.Router()
+router.use(cors())
 
-router.post('/', function(err, fields, files){
-	// handling students login
+mongoose.connect(db_uri, {
+	useNewUrlParser : true,
+	useUnifiedTopology : true
 })
+
+const studentSchema = mongoose.Schema({username:String, password:String})
+const studentModel = mongoose.model('students', studentSchema)
+
+router.post('/login', function(req, res){
+	// handling students login
+	var form = formidable.IncomingForm()
+	form.parse(req, function(err, fields, files){
+		/* --- For analytics purpose, the login history should be recorded later --- */
+		// check if the user name is in the database
+		var username = fields.username
+
+		// for security purpose, this should be encrypted later
+		var password = fields.password
+
+
+		var results = studentModel.find({}, function(err, docs){
+			if(err){
+				console.log(err)
+
+				throw err
+			}
+
+			if(docs.length >= 1){
+				// check if password is correct
+				var result = docs[0]
+				if(result.password == password){
+					console.log('[INFO] Login successfully ...')
+				}else{
+					console.log('[ERROR] Login failed for user ' + result.username + '...')
+				}
+			}else{
+				console.log('[ERROR] Username does not exist ...')
+			}
+		})
+	})
+})
+module.exports = router;
--- client/src/student_home/home.js
+++ client/src/student_home/home.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import './css/StudentLoginForm.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import axios from 'axios'
 
 class StudentLoginForm extends Component {
 	constructor(props){
@@ -27,8 +27,32 @@ class StudentLoginForm extends Component {
 		})
 	}
 
-	onSubmit = () => {
+	onSubmit = (e) => {
 		// submit the two state vars to system
+		e.preventDefault()
+			var username = this.state.username
+			var password = this.state.password
+
+			var formData = new FormData()
+			formData.append('username', username)
+			formData.append('password', password)
+
+			axios({
+				method : 'post',
+				url : 'http://localhost:9000/student/login',
+				data : formData,
+				headers : {
+					'Content-Type' : 'multipart/form-data'
+				}
+			})
+			.then(function(response){
+				// handle success 
+				console.log('Success')
+			})
+			.catch(function(response){
+				// handle fail
+				console.log('Failed')
+			})
 	}
 
 	render() {
